e56bdcafb591346632016ee4a78b873b7ce5ee25
jinan159 <wlsdhks0423@naver.com>
2022-12-06 13:47:54 +0900

    mysql-spatial-index: SpringBootApplication 추가

---
+++ mysql-spatial-index/src/main/java/com/example/mysqlspatialindex/MysqlSpatialIndexApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysqlspatialindex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlSpatialIndexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlSpatialIndexApplication.class, args);
+    }
+
+}
---
+++ mysql-spatial-index/src/test/java/com/example/mysqlspatialindex/MysqlSpatialIndexApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.mysqlspatialindex;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlSpatialIndexApplicationTests {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void contextLoads() {
+    }
+
+}
