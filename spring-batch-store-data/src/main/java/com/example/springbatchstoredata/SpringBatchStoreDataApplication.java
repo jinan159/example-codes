a490baddf7078db81e24d9ea75a447485f9a9a67
jinan159 <wlsdhks0423@naver.com>
2022-12-06 14:23:57 +0900

    spring-batch-store-data: 프로젝트 초기 세팅

---
+++ spring-batch-store-data/src/main/java/com/example/springbatchstoredata/SpringBatchStoreDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbatchstoredata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchStoreDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchStoreDataApplication.class, args);
+	}
+
+}
