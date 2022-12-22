211de630da663ea9a3f5e0c0162e0984b9cf5ca6
jinan159 <wlsdhks0423@naver.com>
2022-12-22 12:51:10 +0900

    inflearn-spring-cloud-msa: discovery-service 의 artifactId 변경
    discovery-service-mvn -> discovery-service

--- inflearn-spring-cloud-msa/discovery-service/src/main/java/com/example/discoveryservice/DiscoveryServiceApplication.java
+++ inflearn-spring-cloud-msa/discovery-service/src/main/java/com/example/discoveryservice/DiscoveryServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.discoveryservicemvn;
+package com.example.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class DiscoveryServiceMvnApplication {
+public class DiscoveryServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiscoveryServiceMvnApplication.class, args);
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
     }
 
 }
